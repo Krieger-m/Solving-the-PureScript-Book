6ac08d3b461c02c1be8d9fd5aa1d85e3689f5c69
Krieger-m <matze.krieger@googlemail.com>
2025-08-20 10:43:03 +0200

    solving chapter3

--- exercises/chapter3/src/Main.purs
+++ exercises/chapter3/src/Main.purs
@@ -3,24 +3,39 @@ module Main
 
 import Prelude
 
-import Data.AddressBook (showAddress, showEntry)
+import Data.AddressBook (Entry, showAddress, showEntry)
+import Data.List (List(..))
 import Effect (Effect)
 import Effect.Console (log)
 
+
+
 firstAddress ∷ { 
   city ∷ String , state ∷ String , street ∷ String }
+
 firstAddress = { city: "Faketown", state: "CA", street: "123 Fake St."}
 
 firstEntry ∷ { 
   address ∷ { city :: String , state :: String , street :: String } , 
   firstName ∷ String , lastName ∷ String }
+
 firstEntry = { firstName: "John", lastName: "Smith", address: firstAddress }
 
+insertEntry ::
+  Entry
+  -> List{ 
+    address   :: { city :: String, state  :: String, street :: String },
+    firstName :: String,
+    lastName  :: String}
+    -> List{ 
+      address   :: { city :: String, state  :: String, street :: String },
+      firstName :: String,
+      lastName  :: String}
+insertEntry e book = Cons e book
 
 
 main :: Effect Unit
 main = do
-  (log("Address: \t" <> showAddress firstAddress <> "🍝"))
-  (log("Entry: \t\t" <> showEntry firstEntry <> "📖"))
+  (log("Address: \t" <> showAddress(firstAddress) <> "🍝"))
+  (log("Entry: \t\t" <> showEntry(firstEntry) <> "📖"))
   
---!x continue here
