c719302209c4c05cf238fa91104364cd237d4f99
Mat <matze.krieger@googlemail.com>
2025-08-21 18:46:11 +0200

    solving chapter3

--- exercises/chapter3/src/Main.purs
+++ exercises/chapter3/src/Main.purs
@@ -3,27 +3,11 @@ module Main
 
 import Prelude
 
-import Data.AddressBook (AddressBook, Entry, emptyBook, showAddress, showEntry, insertEntry)
-import Data.List (List(..))
-import Effect (Effect)
-import Effect.Console (log)
-import Test.Main (book)
+import Effect             (Effect)
+import Effect.Console     (log)
+import Test.MySolutions   (firstEntry, firstAddress,
+                           prt, showAddress, showEntry)
 
-
-
-firstAddress ∷ { 
-  city ∷ String , state ∷ String , street ∷ String }
-
-firstAddress = { city: "Faketown", state: "CA", street: "123 Fake St."}
-
-firstEntry ∷ { 
-  address ∷ { city :: String , state :: String , street :: String } , 
-  firstName ∷ String , lastName ∷ String }
-
-firstEntry = { firstName: "John", lastName: "Smith", address: firstAddress }
-
-bookWithInsertedEntry :: AddressBook
-bookWithInsertedEntry = insertEntry firstEntry emptyBook
 
 
 main :: Effect Unit
@@ -35,15 +19,4 @@ main = do
 
 
 
-{--! solving according to https://book.purescript.org/chapter3.html and some own stuff to get better understanding :) --}
-
-
---------x trying stuff below x--------
-
-onlyTheSecond :: forall  x y. x->y->y
-onlyTheSecond = \a b -> b
-  
-prt :: Effect Unit
-prt =do
-  log("\n\treturning only the second of values: " <> onlyTheSecond 2 "test\n")
 
--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,55 @@ module Test.MySolutions where
 
 import Prelude
 
+import Effect               (Effect)
+import Effect.Console       (log)
+import Data.AddressBook     (Address, AddressBook, Entry, emptyBook, insertEntry)
+
 -- Note to reader: Add your solutions to this file
+
+
+firstAddress ∷ { 
+  city ∷ String , state ∷ String , street ∷ String }
+
+firstAddress = { city: "Faketown", state: "CA", street: "123 Fake St."}
+
+
+firstEntry ∷ { 
+  address ∷ { city :: String , state :: String , street :: String } , 
+  firstName ∷ String , lastName ∷ String }
+
+firstEntry = { firstName: "John", lastName: "Smith", address: firstAddress }
+
+
+bookWithInsertedEntry :: AddressBook
+bookWithInsertedEntry = insertEntry firstEntry emptyBook
+
+
+--?  my own addressbook functions
+
+showAddress :: Address -> String
+showAddress addr =  addr.street <> ", " <>
+                    addr.city   <> ", " <>
+                    addr.state
+
+
+showEntry :: Entry -> String
+showEntry entry =   entry.lastName  <> ", " <>
+                    entry.firstName <> ": " <>
+                    showAddress entry.address
+
+
+
+{--! solving according to https://book.purescript.org/chapter3.html and some own stuff to get better understanding :) --}
+
+
+--------x trying stuff below x--------
+
+
+onlyTheSecond :: forall  x y. x->y->y
+onlyTheSecond = \a b -> b
+  
+prt :: Effect Unit
+prt = do
+  log("\n\treturning only the second of values: " <> onlyTheSecond 2 "test\n")
+
